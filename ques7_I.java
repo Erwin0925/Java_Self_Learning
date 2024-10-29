c6e6cb4fe2d9087dee0fe499c8b5e51d712ef98e
Erwin <tp064564@gmail.com>
2024-10-29 23:59:00 +0800

    question with answer

---
+++ ques7_I.java
@@ -0,0 +1,44 @@
+// Write a Java program to rearrange all the elements of a given array of integers so that all the odd numbers come before all the even numbers.
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;  
+
+public class ques7_I {
+       
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList <Integer> num = new ArrayList<>();
+
+        System.out.println("Plese enter integer 1:");
+        num.add(input.nextInt());
+        System.out.println("Plese enter integer 2:");
+        num.add(input.nextInt());
+        System.out.println("Plese enter integer 3:");
+        num.add(input.nextInt());
+        System.out.println("Plese enter integer 4:");
+        num.add(input.nextInt());
+
+        ArrayList <Integer> even = new ArrayList<>();
+        ArrayList <Integer> odd = new ArrayList<>();
+
+        for (int i = 0; i < num.size(); i++) {
+           if(num.get(i)%2==0){
+                even.add(num.get(i));
+            }else{
+                odd.add(num.get(i));
+            }
+        }
+        Collections.sort(even);
+        even.sort(Comparator.naturalOrder());
+        odd.sort(Comparator.naturalOrder());
+        num.clear();
+        num.addAll(odd);
+        num.addAll(even);
+
+        for(int number : num){
+            System.out.print(number + " ");
+        }
+    }
+        
+}
---
+++ ques7_II.java
@@ -0,0 +1,36 @@
+// Write a Java program to rearrange all the elements of a given array of integers so that all the odd numbers come before all the even numbers.
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;  
+
+public class ques7_II {
+       
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList <Integer> num = new ArrayList<>();
+
+        System.out.print("Plese enter integer 1: ");
+        num.add(input.nextInt());
+        System.out.print("Plese enter integer 2: ");
+        num.add(input.nextInt());
+        System.out.print("Plese enter integer 3: ");
+        num.add(input.nextInt());
+        System.out.print("Plese enter integer 4: ");
+        num.add(input.nextInt());
+
+        Collections.sort(num, (a,b)->{
+            if(a%2==0 && b%2!=0){
+                return 1;
+            }else if (a%2!=0 && b%2==0){
+                return -1;
+            }else{
+                return Integer.compare(a, b);
+            }
+        });
+
+        for(int number : num){
+            System.out.print(number + " ");
+        }
+    }
+        
+}
