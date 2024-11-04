c775266e33c04404ff98d30a2e979f458b65cc59
Erwin <tp064564@gmail.com>
2024-11-04 23:23:28 +0800

    ques

---
+++ ques12.java
@@ -0,0 +1,12 @@
+// Write a Java program to find a number that appears only once in a given array of integers. All numbers occur twice.
+// Example: {10,2,38,23,38,23,21}
+// Output: 23
+
+public class ques12 {
+    public static void main(String[] args) {
+        int[] num = {1,1,2,2,3,4,4,5,5};
+
+        
+
+    }
+}
